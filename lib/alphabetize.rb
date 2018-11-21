85a025f241ae7fd432b39fc926ccd1d7162addd6
Nicholas Rozmus <rozmusnicholas@gmail.com>
2018-11-21 21:50:21 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,16 @@
-def alphabetize(arr)
-  # code here
+#ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+# map each letter of "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz" to a new arrive so you have the index number to use
+
+def alphabetize(array)
+	alpha = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+	alpha_hash = {}
+	alpha.split("").each_with_index do |item, idx|
+		alpha_hash[item] = idx
+	end
+	sorted_array = []
+	sorted_array = array.sort_by { |word|
+		word.chars.map { |c| alpha_hash[c] }
+	}
+	sorted_array
 end
